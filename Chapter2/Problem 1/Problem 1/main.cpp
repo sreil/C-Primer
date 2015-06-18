2ccc737f876b519963eef7a77c795cd70d15a41a
sreil <seanpreilly@gmail.com>
2015-06-18 11:23:18 -0600

    added first problem

---
+++ Chapter2/Problem 1/Problem 1/main.cpp
@@ -0,0 +1,15 @@
+/*Write a C++ program that displays your name and address (or if you value your
+privacy, a fictitious name and address).*/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "My name is Sean" << endl;
+	cout << "My address is 123 Don't Visit Me Lane" << endl;
+
+	return 0;
+}
